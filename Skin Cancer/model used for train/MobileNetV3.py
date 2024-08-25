3d0fe11fd8677da686ebb745715f9b64ee5d36d0
sysll <132797822+sysll@users.noreply.github.com>
2024-08-25 12:24:46 +0800

    Update MobileNetV3.py

--- Skin Cancer/model used for train/MobileNetV3.py
+++ Skin Cancer/model used for train/MobileNetV3.py
@@ -12,7 +12,7 @@ Max = 0.0
 setup_seed(4)
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 # 加载预训练的Inception模型
-model = models.mobilenet_v3_large(weights=None)
+model = models.mobilenet_v2_large(weights=None)
 model.classifier[3] = torch.nn.Linear(1280, 3)  # 将最后的全连接层设置为3分类
 model.aux_logits = False  # 禁用辅助分类器
 model = model.to(device)
@@ -22,7 +22,7 @@ data_dir = 'D:\\Users\\ASUS\\Desktop\\良性癌症等检测'   # 样本地址
 transform1 = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor(),
-    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])  # 因为只有一个通道，所以只需要一个均值和一个标准差
+    transforms.Normalize([0], [1])  # 因为只有一个通道，所以只需要一个均值和一个标准差
 ])
 
 # 构建训练和验证的样本数据集，应用transform
